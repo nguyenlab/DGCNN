4dd746c461cc8b1e08ab423723105681e39e15ac
nguyenlab <nguyenlab.jaist@gmail.com>
2017-10-19 08:05:46 +0100

    remove all

--- src/global.d
+++
@@ -1,8 +0,0 @@
-src/global.d src/global.o: src/global.cpp src/global.h \
- ../CBLAS/include/cblas.h ../CBLAS/include/cblas_f77.h
-
-src/global.h:
-
-../CBLAS/include/cblas.h:
-
-../CBLAS/include/cblas_f77.h:
